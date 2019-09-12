0f0f10f5f2bd0f603c29d76b8ee533d13da341ce
Joao Grassi <joao@tapkey.com>
2019-09-12 16:34:23 +0200

    Added the demo ASP.NET Core Api.

---
+++ src/Demo.Api/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Demo.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MoviesDbContext _dbContext;
+
+        public MoviesController(MoviesDbContext dbContext)
+        {
+            this._dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            var movies = await _dbContext.Movies.ToListAsync();
+
+            return Ok(movies);
+        }
+    }
+}
---
+++ src/Demo.Api/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Demo.Api.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                try
+                {
+                    var context = services.GetRequiredService<MoviesDbContext>();
+                    DbInitializer.Initialize(context, logger);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSentry();
+    }
+}
